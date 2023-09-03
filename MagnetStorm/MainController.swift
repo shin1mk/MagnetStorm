2ca604a0ac0c44af55a19868283667d649bc3016
MK1 <shin.m.dp@gmail.com>
2023-09-03 23:08:27 +0300

    add description text

--- MagnetStorm/MainController.swift
+++ MagnetStorm/MainController.swift
@@ -14,13 +14,13 @@ final class MainController: UIViewController {
     //MARK: Properties
     private let locationManager = CLLocationManager()
     private let geocoder = CLGeocoder()
+    // для текста описания
     private var descriptionText = ""
     private var currentCharacterIndex = 0
 
-    
     private let locationLabel: UILabel = {
         let locationLabel = UILabel()
-        locationLabel.text = "Location"
+        locationLabel.text = "-----"
         locationLabel.font = UIFont.systemFont(ofSize: 30)
         locationLabel.textColor = .white
         locationLabel.textAlignment = .left
@@ -28,28 +28,27 @@ final class MainController: UIViewController {
     }()
     private let geomagneticActivityLabel: UILabel = {
         let geomagneticActivityLabel = UILabel()
-        geomagneticActivityLabel.text = "-"
-        geomagneticActivityLabel.font = UIFont.systemFont(ofSize: 30)
+        geomagneticActivityLabel.text = "G-"
+        geomagneticActivityLabel.font = UIFont.systemFont(ofSize: 45)
         geomagneticActivityLabel.textColor = .white
         geomagneticActivityLabel.textAlignment = .left
         return geomagneticActivityLabel
     }()
     private let descriptionLabel: UILabel = {
         let descriptionLabel = UILabel()
-        descriptionLabel.text = "description"
+        descriptionLabel.text = "----"
         descriptionLabel.font = UIFont.systemFont(ofSize: 30)
         descriptionLabel.textColor = .white
         descriptionLabel.textAlignment = .left
         descriptionLabel.numberOfLines = 0
         return descriptionLabel
     }()
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         setupConstraints()
         setupLocationManager()
-        fetchMagneticDataAndUpdateUI()
-
     }
     //MARK: Constraints
     private func setupConstraints() {
@@ -77,7 +76,7 @@ final class MainController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
-    //MARK: fetchMagneticDataAndUpdateUI
+    //MARK: Fetch Data
     private func fetchMagneticDataAndUpdateUI() {
         fetchMagneticData { [weak self] currentKpValue in
             DispatchQueue.main.async {
@@ -113,48 +112,58 @@ final class MainController: UIViewController {
                 self?.geomagneticActivityLabel.textColor = state.labelColor
 //                self?.descriptionLabel.text = state.descriptionText
                 self?.animateDescriptionLabelAppearance(withText: state.descriptionText)
-
             }
         }
     }
-    
+    //MARK: Animate Description
     private func animateDescriptionLabelAppearance(withText text: String) {
         descriptionText = text
         currentCharacterIndex = 0
         descriptionLabel.text = ""
-
         // Запускаем таймер, чтобы добавлять буквы каждые 0.1 секунды
-        Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { [weak self] timer in
+        Timer.scheduledTimer(withTimeInterval: 0.02, repeats: true) { [weak self] timer in
             guard let self = self else {
                 timer.invalidate()
                 return
             }
-
             if self.currentCharacterIndex < self.descriptionText.count {
                 let index = self.descriptionText.index(self.descriptionText.startIndex, offsetBy: self.currentCharacterIndex)
                 let character = self.descriptionText[index]
                 self.descriptionLabel.text?.append(character)
                 self.currentCharacterIndex += 1
             } else {
-                // Весь текст добавлен, останавливаем таймер
-                timer.invalidate()
+                timer.invalidate() // Весь текст добавлен, останавливаем таймер
             }
         }
     }
-
 } // end
-//MARK: - LocationManagerDelegate
+//MARK: - Location Manager Delegate
 extension MainController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             geocoder.reverseGeocodeLocation(location) { [weak self] (placemarks, error) in
                 if let placemark = placemarks?.first {
                     if let city = placemark.locality, let country = placemark.country {
-                        // Обновите ваш locationLabel
-                        self?.locationLabel.text = "\(city), \(country)"
+                        self?.locationLabel.text = "\(city), \(country)" // Обновите ваш locationLabel
+                        self?.fetchMagneticDataAndUpdateUI() // запустится только после разрешения геолокации
                     }
                 }
             }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            print("Доступ к геопозиции разрешен при использовании приложения.")
+        case .denied:
+            print("Доступ к геопозиции запрещен.")
+        case .restricted:
+            print("Доступ к геопозиции ограничен.")
+        case .notDetermined:
+            print("Статус разрешения на доступ к геопозиции еще не определен.")
+        default:
+            break
         }
     }
 }
@@ -175,25 +184,25 @@ extension MainController {
         
         var labelText: String {
             switch self {
-            case .noStorm:
+            case .noStorm: // 0
                 return "G0"
-            case .minorStorm:
+            case .minorStorm: // 1
                 return "G1"
-            case .weakStorm:
+            case .weakStorm: // 2
                 return "G2"
-            case .moderateStorm:
+            case .moderateStorm: // 3
                 return "G3"
-            case .strongStorm:
+            case .strongStorm: // 4
                 return "G4"
-            case .severeStorm:
+            case .severeStorm: // 5
                 return "G5"
-            case .extremeStorm:
+            case .extremeStorm: // 6
                 return "G6"
-            case .outstandingStorm:
+            case .outstandingStorm: // 7
                 return "G7"
-            case .exceptionalStorm:
+            case .exceptionalStorm: // 8
                 return "G8"
-            case .superstorm:
+            case .superstorm: // 9
                 return "G9"
             case .unknown:
                 return "?"
@@ -203,25 +212,25 @@ extension MainController {
         var descriptionText: String {
             switch self {
             case .noStorm:
-                return "Отсутствие бури:\nНет активности магнитных бурь, магнитное поле Земли стабильно."
+                return "Отсутствие бури:\nВлияние на организм человека практически отсутствует. Люди не ощущают никаких физических или эмоциональных изменений из-за отсутствия магнитных бурь."
             case .minorStorm:
-                return "Слабая буря:\nНебольшое возмущение магнитного поля. Возможны слабые влияния на системы связи и навигации."
+                return "Слабая буря:\nВлияние на человека очень незначительное. Некоторые люди с повышенной чувствительностью к магнитным полям могут замечать легкое беспокойство."
             case .weakStorm:
-                return "Умеренная буря:\nУмеренное возмущение магнитного поля. Может вызывать более серьезные сбои в работе систем связи и навигации, особенно на высоких широтах."
+                return "Умеренная буря:\nВлияние на организм человека все равно остается небольшим. Некоторые люди могут испытывать головные боли или изменения в сне."
             case .moderateStorm:
-                return "Сильная буря:\nСильное возмущение магнитного поля. Может вызывать проблемы с работой спутников и систем связи, а также создавать опасность для космических аппаратов."
+                return "Сильная буря:\nЭтот уровень бури может повысить вероятность появления физических и эмоциональных симптомов у большинства людей. Возможны головные боли, бессонница, ухудшение настроения у чувствительных людей."
             case .strongStorm:
-                return "Очень сильная буря:\nОчень сильное возмущение магнитного поля. Этот уровень может вызвать серьезные проблемы для спутниковых систем и сетей электропередачи."
+                return "Очень сильная буря:\nЭтот уровень бури может повысить вероятность появления физических и эмоциональных симптомов у большинства людей. Возможны головные боли, бессонница, ухудшение настроения особенно у чувствительных людей."
             case .severeStorm:
-                return "Сильнейшая буря:\nСамый высокий уровень активности. Может вызывать критические сбои в работе спутников и технических систем, а также повышенные риски для космических аппаратов."
+                return "Сильнейшая буря:\nВлияние на человека на этом уровне может стать более заметным. Могут усилиться симптомы, такие как бессонница, головные боли и нервозность у некоторых людей."
             case .extremeStorm:
-                return "Буря выдающегося масштаба:\nЭто экстремально сильная магнитная буря, которая может вызвать массовые отключения электроэнергии, повреждение спутников и серьезные технические проблемы."
+                return "Буря выдающегося масштаба:\nНа этом уровне симптомы могут стать более выраженными и распространенными. У людей, более чувствительных к магнитным полям, могут возникать более серьезные головные боли, бессонница и изменения настроения."
             case .outstandingStorm:
-                return "Буря исключительного масштаба:\nНаивысший уровень активности магнитных бурь. Может создавать катастрофические последствия для систем связи, навигации и электропередачи."
+                return "Буря исключительного масштаба:\nЭтот уровень бури может вызвать значительное ухудшение физического и эмоционального состояния у большого числа людей. Головные боли, бессонница, нервозность и ухудшение настроения могут наблюдаться в значительной степени."
             case .exceptionalStorm:
-                return "Сверхбуря:\nЭтот уровень обозначает сверхбурю масштаба апокалипсиса, которая имеет потенциал разрушить множество технических систем и причинить значительный ущерб."
+                return "Сверхбуря:\nНа этом уровне возможны самые серьезные и неопределенные воздействия на человека. Могут возникать сильные головные боли, усиление бессонницы и серьезное изменение эмоционального состояния."
             case .superstorm:
-                return "Супербуря:\nСамый высший уровень активности магнитных бурь, с катастрофическими последствиями для всего технического оборудования и систем связи."
+                return "Супербуря:\nСамый высший уровень активности магнитных бурь, с катастрофическими последствиями для всего организма и технического оборудования в мире."
             case .unknown:
                 return "Неизвестно"
             }
@@ -230,67 +239,28 @@ extension MainController {
         var labelColor: UIColor {
             switch self {
             case .noStorm:
-                return UIColor.systemGreen
+                return UIColor.systemGreen // 0
             case .minorStorm:
-                return UIColor.systemGreen
+                return UIColor.systemGreen // 1
             case .weakStorm:
-                return UIColor.systemGreen
+                return UIColor.systemGreen // 2
             case .moderateStorm:
-                return UIColor.systemYellow
+                return UIColor.systemYellow // 3
             case .strongStorm:
-                return UIColor.systemRed
+                return UIColor.systemRed // 4
             case .severeStorm:
-                return UIColor.systemRed
+                return UIColor.systemRed // 5
             case .extremeStorm:
-                return UIColor.systemBlue
+                return UIColor.systemBlue // 6
             case .outstandingStorm:
-                return UIColor.systemRed
+                return UIColor.systemRed // 7
             case .exceptionalStorm:
-                return UIColor.systemRed
+                return UIColor.systemRed // 8
             case .superstorm:
-                return UIColor.systemRed
+                return UIColor.systemRed // 9
             case .unknown:
-                return UIColor.white
+                return UIColor.white // 0
             }
         }
     }
-
 }
-//MARK: Video
-//    private func setupVideoBackground() {
-//        guard let videoURL = Bundle.main.url(forResource: "video_background", withExtension: "mp4") else {
-//            print("Failed to locate video file.")
-//            return
-//        }
-//
-//        let videoPlayer = AVPlayer(url: videoURL)
-//        let videoLayer = AVPlayerLayer(player: videoPlayer)
-//
-//        videoLayer.videoGravity = .resizeAspectFill
-//        videoLayer.frame = view.bounds
-//        // Добавляем видеослой как нижний слой
-//        view.layer.insertSublayer(videoLayer, at: 0)
-//
-//        videoPlayer.play()
-//    }
-// Теперь добавляем слой для видеофона прям в констрейнты вниз
-//        setupVideoBackground()
-
-//анимация плавного появления
-// это во вью дид лоад
-//descriptionLabel.alpha = 0.0 // Начально скрываем label
-//
-//private func animateDescriptionLabelAppearance(withText text: String) {
-//    descriptionLabel.text = "" // Очищаем текст перед анимацией
-//    descriptionLabel.alpha = 0.0 // Начинаем с нулевой прозрачности
-//
-//    // Создаем анимацию появления текста по буквам
-//    UIView.animate(withDuration: 5.0, delay: 0.0, options: [.curveLinear], animations: { [weak self] in
-//        text.forEach { char in
-//            self?.descriptionLabel.text?.append(char)
-//            self?.descriptionLabel.alpha = 1.0
-//        }
-//    }, completion: nil)
-//}
-//self?.animateDescriptionLabelAppearance(withText: state.descriptionText)
-
---
+++ MagnetStorm/Views/text.swift
@@ -0,0 +1,98 @@
+//
+//  text.swift
+//  MagnetStorm
+//
+//  Created by SHIN MIKHAIL on 03.09.2023.
+//
+
+/*
+ G0 (Отсутствие бури): Влияние на организм человека практически отсутствует. Люди не ощущают никаких физических или эмоциональных изменений из-за отсутствия магнитных бурь.
+ G1 (Слабая буря): Влияние на человека очень незначительное. Некоторые люди с повышенной чувствительностью к магнитным полям могут замечать легкое беспокойство.
+ G2 (Умеренная буря): Влияние на организм человека все равно остается небольшим. Некоторые люди могут испытывать головные боли или изменения в сне.
+ G3 (Сильная буря): Этот уровень бури может повысить вероятность появления физических и эмоциональных симптомов у большинства людей. Возможны головные боли, бессонница, ухудшение настроения у чувствительных людей.
+ G4 (Очень сильная буря): Этот уровень бури может повысить вероятность появления физических и эмоциональных симптомов у большинства людей. Возможны головные боли, бессонница, ухудшение настроения особенно у чувствительных людей.
+ G5 (Сильнейшая буря): Влияние на человека на этом уровне может стать более заметным. Могут усилиться симптомы, такие как бессонница, головные боли и нервозность у некоторых людей.
+ G6 (Буря выдающегося масштаба): На этом уровне симптомы могут стать более выраженными и распространенными. У людей, более чувствительных к магнитным полям, могут возникать более серьезные головные боли, бессонница и изменения настроения.
+ G7 (Буря исключительного масштаба): Этот уровень бури может вызвать значительное ухудшение физического и эмоционального состояния у большого числа людей. Головные боли, бессонница, нервозность и ухудшение настроения могут наблюдаться в значительной степени.
+ G8 (Сверхбуря): На этом уровне возможны самые серьезные и неопределенные воздействия на человека. Могут возникать сильные головные боли, усиление бессонницы и серьезное изменение эмоционального состояния.
+ 
+ 
+ укр
+ G0 (Відсутність бурі): Вплив на організм людини практично відсутній. Люди не відчувають ніяких фізичних або емоційних змін через відсутність магнітних бурь.
+ G1 (Слабка буря): Вплив на людину дуже незначний. Деякі люди з підвищеною чутливістю до магнітних полів можуть помічати легку тривожність.
+ G2 (Помірна буря): Вплив на організм людини все ж залишається незначним. Деякі люди можуть відчувати головні болі або зміни у сні.
+ G3 (Сильна буря): Цей рівень бурі може підвищити ймовірність фізичних і емоційних симптомів у більшості людей. Можливі головні болі, безсоння, погіршення настрою у чутливих людей.
+ G4 (Дуже сильна буря): Цей рівень бурі також може підвищити ймовірність фізичних і емоційних симптомів у більшості людей. Можливі головні болі, безсоння та погіршення настрою, особливо у чутливих людей.
+ G5 (Найсильніша буря): Вплив на людину на цьому рівні може стати помітнішим. Можуть підсилитися симптоми, такі як безсоння, головні болі та нервозність у деяких людей.
+ G6 (Буря надзвичайного масштабу): На цьому рівні симптоми можуть стати більш виразними та поширеними. У людей, які більше чутливі до магнітних полів, можуть виникати серйозні головні болі, безсоння та зміни настрою.
+ G7 (Буря виняткового масштабу): Цей рівень бурі може спричинити значне погіршення фізичного та емоційного стану багатьох людей. Головні болі, безсоння, нервозність та погіршення настрою можуть спостерігатися в значній мірі.
+ G8 (Супербуря): На цьому рівні можливі найсерйозніші та непередбачувані впливи на людину. Можуть виникати сильні головні болі, загострення безсоння та серйозні зміни емоційного стану.
+ 
+ 
+ eng
+ G0 (No Storm): The impact on the human body is practically nonexistent. People do not experience any physical or emotional changes due to the absence of magnetic storms.
+ G1 (Minor Storm): The influence on a person is very slight. Some individuals with heightened sensitivity to magnetic fields may notice mild discomfort.
+ G2 (Moderate Storm): The impact on the human body remains small. Some people may experience headaches or changes in sleep patterns.
+ G3 (Strong Storm): This level of storm can increase the likelihood of physical and emotional symptoms in most individuals. Headaches, insomnia, and worsened mood are possible, especially in sensitive individuals.
+ G4 (Severe Storm): This level of storm can also increase the likelihood of physical and emotional symptoms in most individuals. Headaches, insomnia, and worsened mood are possible, particularly in sensitive individuals.
+ G5 (Extreme Storm): The impact on a person can become more noticeable at this level. Symptoms such as insomnia, headaches, and nervousness may intensify in some individuals.
+ G6 (Exceptional Storm): At this level, symptoms can become more pronounced and widespread. People who are more sensitive to magnetic fields may experience more severe headaches, insomnia, and mood swings.
+ G7 (Exceptional Storm): This level of storm can cause significant deterioration in the physical and emotional state of a large number of people. Headaches, insomnia, nervousness, and worsened mood can be observed to a considerable extent.
+ G8 (Superstorm): At this level, the most serious and unpredictable effects on a person are possible. Severe headaches, intensified insomnia, and significant changes in emotional state may occur.
+
+ 
+ //MARK: Video
+ //    private func setupVideoBackground() {
+ //        guard let videoURL = Bundle.main.url(forResource: "video_background", withExtension: "mp4") else {
+ //            print("Failed to locate video file.")
+ //            return
+ //        }
+ //
+ //        let videoPlayer = AVPlayer(url: videoURL)
+ //        let videoLayer = AVPlayerLayer(player: videoPlayer)
+ //
+ //        videoLayer.videoGravity = .resizeAspectFill
+ //        videoLayer.frame = view.bounds
+ //        // Добавляем видеослой как нижний слой
+ //        view.layer.insertSublayer(videoLayer, at: 0)
+ //
+ //        videoPlayer.play()
+ //    }
+ // Теперь добавляем слой для видеофона прям в констрейнты вниз
+ //        setupVideoBackground()
+ //MARK: animation
+ //анимация плавного появления
+ // это во вью дид лоад
+ //descriptionLabel.alpha = 0.0 // Начально скрываем label
+ //
+ //private func animateDescriptionLabelAppearance(withText text: String) {
+ //    descriptionLabel.text = "" // Очищаем текст перед анимацией
+ //    descriptionLabel.alpha = 0.0 // Начинаем с нулевой прозрачности
+ //
+ //    // Создаем анимацию появления текста по буквам
+ //    UIView.animate(withDuration: 5.0, delay: 0.0, options: [.curveLinear], animations: { [weak self] in
+ //        text.forEach { char in
+ //            self?.descriptionLabel.text?.append(char)
+ //            self?.descriptionLabel.alpha = 1.0
+ //        }
+ //    }, completion: nil)
+ //}
+ //self?.animateDescriptionLabelAppearance(withText: state.descriptionText)
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+*/
